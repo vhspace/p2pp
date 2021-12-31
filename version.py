881323308e4c41b00b8f21832793b77af90ac5f3
tomvandeneede <t.vandeneede@pandora.be>
2021-12-31 07:26:03 +0100

    8.0.17 - order of gcode generation changed to avoid blob

--- version.py
+++ version.py
@@ -139,15 +139,15 @@ releaseinfo = {
     "8.0.13": "Replace G4 S0 by M400 - also PS generated.",
     "8.0.14": "Correct location of .MAF / .MSF files.",
     "8.0.15": "Correct Z height after in PS2.4 in case of empty after purge.",
-
     "8.0.16": "Corrected project name for P2 output.",
+    "8.0.17": "Corrected order of gcode inserted to avoid blob.",
     '--- RELEASE INFORMATION': 'END'
 }
 
 # general version info
 MajorVersion = 8
 MinorVersion = 0
-Build = 16
+Build = 17
 
 Version = "{}.{:02}.{:02}".format(MajorVersion, MinorVersion, Build)
 
